9eaf30f9d3891538861b3f4d2619cbf1dde327f0
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-01 15:40:48 +0530

    Admin autocomplete paper

--- library_backend/papers/admin.py
+++ library_backend/papers/admin.py
@@ -6,13 +6,14 @@ from .models import Course, Paper
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name','course_id')
+    search_fields = ('name','course_id')
 
 @admin.register(Paper)
 class PaperAdmin(admin.ModelAdmin):
     list_display = ('course','semester','year')
     list_filter = ('course','semester','year')
     search_fields = ('course__name','course__course_id','semester','year')
-
+    autocomplete_fields = ('course',)
     actions = ['hide_papers']
 
     @admin.action(description="Hide selected papers")
