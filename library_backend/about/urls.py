d8d8f86fe19c10f09f291b25fdebd0c82067847e
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-01 15:17:08 +0530

    Feedback api removed

--- library_backend/about/urls.py
+++ library_backend/about/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("rules_regulations/",LibraryRulesAndRegulationAPI.as_view()),
     path("committee/",LibraryCommitteeAPI.as_view()),
     path("team/",LibraryTeamAPI.as_view()),
-    path("feedback/",FeedbackListAPI.as_view()),
     path("brochure/",LibraryBrochureAPI.as_view()),
     path("timings/",LibraryTimingsAPI.as_view()),
     path("desk/",LibrarianDeskAPI.as_view()),
