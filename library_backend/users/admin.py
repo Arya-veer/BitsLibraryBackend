dff1e0cd307f37ae8e3ebabaad414fa2179d6b3d
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-02 01:50:36 +0530

    change

--- library_backend/users/admin.py
+++ library_backend/users/admin.py
@@ -32,8 +32,8 @@ class UserProfileResource(resources.ModelResource):
         skip_unchanged = True
         report_skipped = True
         exclude = ("id",)
-        use_bulk = True
-        batch_size= 1000
+        # use_bulk = True
+        # batch_size= 1000
         skip_html_diff= True
         
 
