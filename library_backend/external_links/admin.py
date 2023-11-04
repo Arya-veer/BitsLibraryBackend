f0dbf43aa6f2250bd4d066fd2902eb06aa4211aa
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-04 15:41:46 +0530

    Admin link site

--- library_backend/external_links/admin.py
+++ library_backend/external_links/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
-admin.site.register(LinkSite)
+# admin.site.register(LinkSite)
+
+@admin.register(LinkSite)
+class LinkSiteAdmin(admin.ModelAdmin):
+    list_display = ("name","url")
+    search_fields = ("name","url")
+    list_filter = ("link_type",)
 
