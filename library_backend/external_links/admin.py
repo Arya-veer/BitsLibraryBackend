cbda90bfbf2d1678641b355dd065215547ca2595
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-04 15:44:46 +0530

    Fix admin

--- library_backend/external_links/admin.py
+++ library_backend/external_links/admin.py
@@ -6,7 +6,7 @@ from .models import *
 
 @admin.register(LinkSite)
 class LinkSiteAdmin(admin.ModelAdmin):
-    list_display = ("name","url")
-    search_fields = ("name","url")
+    list_display = ("site_name","url")
+    search_fields = ("site_name","url")
     list_filter = ("link_type",)
 
