48b3262f887b330f1afee5bc3d1fa5203a877d52
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-09 16:25:50 +0530

    url static id misc websitetext

--- library_backend/misc/urls.py
+++ library_backend/misc/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("home/",HomePageAPI.as_view()),
     path("faqs/",FreqAskedQuestionsListAPI.as_view()),
     path("feedback/",FeedbackListCreateAPI.as_view()),
-    path("text/<str:title>/",WebsiteTextRetrieveAPI.as_view()),
+    path("text/<str:static_id>/",WebsiteTextRetrieveAPI.as_view()),
 ]
