06e7ea18a50b6c754dc069ad97161dc9d1c31a89
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-18 18:39:56 +0530

    Add filter for course type in CourseList view

--- library_backend/papers/views.py
+++ library_backend/papers/views.py
@@ -14,7 +14,11 @@ class CourseList(generics.ListAPIView):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        qs = qs.filter(id__in = Paper.objects.all().values_list('course',flat=True))
+        course_type = self.request.query_params.get('course_type',None)
+        if course_type is not None and course_type == 'paper':
+            qs = qs.filter(id__in = Paper.objects.all().values_list('course',flat=True))
+        elif course_type is not None and course_type == 'textbook':
+            qs = qs.filter(id__in = TextBook.objects.all().values_list('course',flat=True))
         search = self.request.query_params.get('search',None)
         if search is not None:
             qs = qs.filter(name__icontains=search) | qs.filter(course_id__icontains=search)
