0a95c4fcb4e32672e15363d74e0678539f173c4f
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-11-24 19:23:13 +0530

    StateFilter

--- library_backend/papers/admin.py
+++ library_backend/papers/admin.py
@@ -3,11 +3,39 @@ from django.contrib import admin
 # Register your models here.
 from .models import Course, Paper,TextBook
 
+
+class StateFilter(admin.SimpleListFilter):
+    title = 'State'
+
+    parameter_name = 'state'
+
+    def lookups(self, request, model_admin):
+        lookup_data = [
+            ("Has Papers", "Has Papers"),
+            ("Has TextBooks", "Has TextBooks"),
+            ("Has Both", "Has Both"),
+            ("Has Nothing", "Has Nothing"),
+        ]
+
+        return lookup_data
+
+    def queryset(self, request, queryset):
+        if self.value() == "Has Papers":
+            return queryset.filter(id__in = Paper.objects.all().values_list('course',flat=True))
+        elif self.value() == "Has TextBooks":
+            return queryset.filter(id__in = TextBook.objects.all().values_list('course',flat=True))
+        elif self.value() == "Has Both":
+            return queryset.filter(id__in = Paper.objects.all().values_list('course',flat=True)).filter(id__in = TextBook.objects.all().values_list('course',flat=True))
+        elif self.value() == "Has Nothing":
+            return queryset.exclude(id__in = Paper.objects.all().values_list('course',flat=True)).exclude(id__in = TextBook.objects.all().values_list('course',flat=True))
+        else:
+            return queryset
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name','course_id','campus')
     search_fields = ('name','course_id')
-    list_filter = ('campus',)
+    list_filter = ('campus',StateFilter,)
 
 @admin.register(Paper)
 class PaperAdmin(admin.ModelAdmin):
