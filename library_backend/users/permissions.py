8f86e7fef760eba8a21c011776cf8aa122292fe5
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2024-01-31 22:38:02 +0530

    Staff room booking

---
+++ library_backend/users/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+from .models import UserProfile
+
+class StaffPermission(permissions.BasePermission):
+    """
+    Custom permission to only allow staff members to access it.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and UserProfile.objects.filter(auth_user=request.user,user_type="Staff").exists()
