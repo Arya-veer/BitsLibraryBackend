c943ab42dae25d16362760bed44c268f74aba691
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2024-02-07 11:24:38 +0530

    id change

--- library_backend/booking/views.py
+++ library_backend/booking/views.py
@@ -159,7 +159,7 @@ class BookingDetailAPI(generics.RetrieveAPIView):
     def get_object(self):
 
         booking = Booking.objects.get(id = self.kwargs['id'])
-        if booking.booker != self.request.user.profile:
+        if booking.booker.user_type != "Staff" and booking.booker != self.request.user.profile:
             raise Booking.DoesNotExist
         return booking
     
