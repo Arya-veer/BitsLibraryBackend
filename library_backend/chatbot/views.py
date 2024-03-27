d27cd857c227d7e0a42b45f180558384f2285ec1
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2024-03-27 17:59:18 +0530

    changes

--- library_backend/chatbot/views.py
+++ library_backend/chatbot/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from rest_framework import views,status
+from rest_framework.response import Response
 # Create your views here.
+
+
+class AskQuestionAPI(views.APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        question = request.data.get("question")
+        if not question:
+            return Response({"error":"Please provide a question."},status = status.HTTP_400_BAD_REQUEST)
+        
