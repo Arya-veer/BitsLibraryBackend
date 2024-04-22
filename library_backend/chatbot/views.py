cf987a142abdfea71fb7a754a7dafe2925741b76
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2024-04-22 17:20:24 +0530

    chatbot fix

--- library_backend/chatbot/views.py
+++ library_backend/chatbot/views.py
@@ -2,7 +2,9 @@ from rest_framework import views,status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
-from library_backend.settings import CHATBOT
+# from library_backend.settings import CHATBOT
+from chatbot.chatbot import ChatBot
+
 # Create your views here.
 
 
@@ -13,7 +15,7 @@ class AskQuestionAPI(views.APIView):
         question = request.data.get("question")
         if not question:
             return Response({"answer":"Hey! How can I help you?"},status = status.HTTP_400_BAD_REQUEST)
-        chatbot = CHATBOT
+        chatbot = ChatBot.get_object()
         if chatbot is None:
             return Response({"answer":"Chatbot is not initialized."},status = status.HTTP_400_BAD_REQUEST)
         try:
